0ad5e0ed15ae681493d15a2e4097b36714f52c2e
Lucas Gonzaga Pereira <lucasgpec@gmail.com>
2025-07-05 00:27:12 -0300

    Adiciona domínio do Render ao ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zhv*=h@gokr06g)w4%y)57be5xm*6!-^o9$13c6c&h7253kr5!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sistema-clubevizinorte.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
