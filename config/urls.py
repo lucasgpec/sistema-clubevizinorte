0f476b8cb1e15c2e84cb24f7bae8e4b522264b75
Lucas Gonzaga Pereira <lucasgpec@gmail.com>
2025-07-05 00:38:42 -0300

    Adiciona página inicial simples na rota /

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Bem-vindo ao sistema do clube!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # Página inicial simples
 ]
